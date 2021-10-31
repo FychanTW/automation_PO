314081aa3354f67d7117e6be3648d0cef72b2e69
CHAN_R723 <fychan@d.phys.nagoya-u.ac.jp>
2021-10-31 18:46:48 +0900

    daily commit

--- src/AutomationPO_GUI_Setup.py
+++ src/AutomationPO_GUI_Setup.py
@@ -3,7 +3,6 @@ from AutomationPO_MainWindow import Ui_MainWindow
 from AboutWindow_GUI import Ui_AboutWindow
 import GUILib
 class MainGUI(Ui_MainWindow):
-    print("new class")
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
         self.actionAbout.triggered.connect(self.openAboutWindow)
@@ -23,13 +22,25 @@ class MainGUI(Ui_MainWindow):
         self.window.show()
 
     def loadPO(self):
-        GUILib.loadPO(self)
+        PO_to_be_process = PO_information()
+        PO_to_be_process = GUILib.loadPO(self)
 
     def loadToken(self):
-        Token = GUILib.loadToken(self)
+        self.Token = 'not loaded'
+        self.Token = GUILib.loadToken(self)
 
     def extractPO(self):
         GUILib.extractPO(self)
 
-    def pushTrello(self,Token):
-        GUILib.pushTrello(self,Token)
+    def pushTrello(self):
+        #Token = GUILib.loadToken(self)
+        if self.Token != 'not loaded':
+            GUILib.pushTrello(self)
+
+class PO_information(object):
+  def __init__(self):
+    self.PO_filename = []
+    self.PO_number = []
+    self.PO_date = []
+    self.PO_price = 0
+    self.PO_email = []
--- src/GUILib.py
+++ src/GUILib.py
@@ -1,6 +1,17 @@
 from PyQt5 import QtWidgets
 import os,fnmatch
 import pdf2txt as p2t
+from trello import TrelloClient
+
+
+class PO_information(object):
+  def __init__(self):
+    self.PO_filename = []
+    self.PO_number = []
+    self.PO_date = []
+    self.PO_price = 0
+    self.PO_email = []
+    self.PO_status = []
 
 #initial check1: create necessary folder if not found
 #initial check2: move unprocessed files to backup folder and create log 
@@ -13,12 +24,13 @@ def initial_check(arg):
 def loadPO(arg):
     tmp_dir = os.getcwd()
     dirname = QtWidgets.QFileDialog.getExistingDirectory(arg,'Select PO folder:',tmp_dir)
-    listofFiles = os.listdir(dirname)
+    if dirname != '':
+        listofFiles = os.listdir(dirname)
     #print(listofFiles)
-    arg.model.setStringList(listofFiles)
-    arg.PO_listView.setModel(arg.model)
-    arg.CurrentDirLabel.setText(repr(dirname))
-    convertPO(arg,listofFiles,dirname)
+        arg.model.setStringList(listofFiles)
+        arg.PO_listView.setModel(arg.model)
+        arg.CurrentDirLabel.setText(repr(dirname))
+        convertPO(arg,listofFiles,dirname)
 
 #loadToken: load Trello API keys and token
 def loadToken(arg):
@@ -40,7 +52,6 @@ def convertPO(arg,listOfFiles,dirname):
             #print(tmp_name)
             p2t.main(['-o'+'.\\extracted_text\\'+tmp_name,'.\\sample_PO\\' + entry])
             extractPO(arg,tmp_name)
-        #os.rename('.\\'+entry,'..\\archive_PO\\'+'done_'+entry)
 
 #extractPO: extract the PO number from the text file
 #extractPO: display the PO number on Qt PONum_listView
@@ -62,4 +73,32 @@ def extractPO(arg,textfilename):
     arg.PONum_listView.setModel(arg.PONum_model)
 
 def pushTrello(arg):
-    print("")
+    if arg.Token != 'not loaded':
+            client = TrelloClient(
+            api_key = arg.Token[0],
+            token= arg.Token[1] 
+            )
+    all_board = client.list_boards()
+    #print(all_board)
+    BOARD_ID = all_board[0].id
+    board = client.get_board(BOARD_ID)
+    PO_lists = board.all_lists()
+    """
+    for item in range(6):
+        print(PO_lists[item].name)
+        print(PO_lists[item].id)
+    """
+    ordered_PO_list = board.get_list(PO_lists[0].id)
+    PO_to_push = arg.PONum_model.stringList()
+    for item in PO_to_push:
+        #create_card_result = ordered_PO_list.add_card(item)
+        print()
+
+    #archivePO()
+
+def archivePO():
+    tmp_dir = os.getcwd()
+    os.chdir('.\\sample_PO')
+    listOfFiles = os.listdir('.\\')
+    for entry in listOfFiles:
+        os.rename('.\\'+entry,'..\\archive_PO\\'+'done_'+entry)
